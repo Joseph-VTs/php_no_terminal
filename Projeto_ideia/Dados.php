e6eb82104e79990c86061600015b3dcd070ba305
Joseph-VTs <compcopycode@gmail.com>
2026-07-20 15:41:43 -0300

    Troquei o nome da  para John Deere, e acrescentei o ; assim como no  o mesmo campo foi adicionado

--- Projeto_ideia/Dados.php
+++ Projeto_ideia/Dados.php
@@ -4,11 +4,12 @@ $usuario_salvo = "Jose";
 $senha_salva = "123";
 
 // Acesso da Empresa
-$empresa_nome_salvo = "Mercado";
+$empresa_nome_salvo = "John Deere";
 
 // Acesso Funcionários
 $funcionario_nome_salvo = "Maria";
 $funcionario_senha_salvo = "123";
+$funcionario_tipo_salvo = "Normal";
 
 
 $meu_banco = [
