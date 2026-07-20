941271e8a70ce1c1677238612c76a6f51a7100a1
Joseph-VTs <compcopycode@gmail.com>
2026-07-20 14:08:21 -0300

    Criando arquivo dos acessos ao sistema - Públicos

---
+++ Projeto_ideia/Dados.php
@@ -0,0 +1,23 @@
+<?php
+// ADM
+$usuario_salvo = "Jose";
+$senha_salva = "123";
+
+// Acesso da Empresa
+$empresa_nome_salvo = "Mercado";
+
+// Acesso Funcionários
+$funcionario_nome_salvo = "Maria";
+$funcionario_senha_salvo = "123";
+
+
+$meu_banco = [
+    'Clientes' => [
+        'Nome' => [],
+        'Senha' => []
+    ],
+    'Funcionarios' => [
+        'Nome' => [],
+        'Senha' => []
+    ],
+];
